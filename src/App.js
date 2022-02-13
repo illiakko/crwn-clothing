079d58d303a7a653c40d7fe9e7f225262a5cd1b9
Ilya <ilya.s.kozachenko@gmail.com>
2022-02-13 20:42:59 +0200

    updated app with aa shop page, collection preview component and collection item component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 import "./App.css";
-import HomePage from "./pages/homepage/homepage.component.jsx";
 
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-)
+import HomePage from "./pages/homepage/homepage.component.jsx";
+import ShopPage from './pages/shop/shop.component.jsx'
+
+
 
 function App() {
   return (
@@ -15,7 +13,7 @@ function App() {
     <div className="App">
       <Switch>
         <Route exact path='/' component={ HomePage } />
-        <Route exact path='/shop/hats' component={ HatsPage } />
+        <Route exact path='/shop' component={ ShopPage } />
 
       </Switch>
     </div>
